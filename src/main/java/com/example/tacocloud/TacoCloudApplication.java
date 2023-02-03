ba4620180a7ef8d5748c389a7de12621faa80deb
alezalez8 <5446943@ukr.net>
2023-02-03 23:19:00 +0200

    111

--- src/main/java/com/example/tacocloud/TacoCloudApplication.java
+++ src/main/java/com/example/tacocloud/TacoCloudApplication.java
@@ -3,7 +3,7 @@ package com.example.tacocloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  // @SpringBootConfiguration + @EnableAutoConfiguration + @ComponentScan
 public class TacoCloudApplication {
 
     public static void main(String[] args) {
@@ -11,3 +11,6 @@ public class TacoCloudApplication {
     }
 
 }
+
+//    в командной строке наберите  netstat -o -n -a | findstr 0.0:8080
+//    и потом в диспетчере задач в службах по ИД процесса найдите службу
